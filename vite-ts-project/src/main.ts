e52c524463b4f24ee27afe33fcc58af1dd0bce22
mrsherpacode <sherpacoding@gmail.com>
2026-07-28 15:05:28 -0500

    completed typescript project

--- vite-ts-project/src/main.ts
+++ vite-ts-project/src/main.ts
@@ -1,58 +1,37 @@
 import "./css/style.css";
-import typescriptLogo from "./assets/typescript.svg";
-import viteLogo from "./assets/vite.svg";
+import FullList from "./modal/FullList";
+import listItem from "./modal/ListItem";
+import ListTemplate from "./templates/ListTemplates";
+const initApp = (): void => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
+  const itemIntryForm = document.getElementById(
+    "itemEntryForm",
+  ) as HTMLFormElement;
+  itemIntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault();
+    const input = document.getElementById("newItem") as HTMLInputElement;
+    const newEntryText: string = input.value.trim();
+    if (!newEntryText.length) return;
+    const itemId: number = fullList.List.length
+      ? parseInt(fullList.List[fullList.List.length - 1].id) + 1
+      : 1;
+    const newItem = new listItem(itemId.toString(), newEntryText);
+    fullList.addItem(newItem);
+    template.render(fullList);
+  });
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+  const clearItems = document.getElementById(
+    "clearItemsButton",
+  ) as HTMLButtonElement;
 
-<div class="ticks"></div>
+  clearItems.addEventListener("click", (): void => {
+    fullList.clearList();
+    template.clear();
+  });
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+  fullList.load();
+  template.render(fullList);
+};
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+document.addEventListener("DOMContentLoaded", initApp);
